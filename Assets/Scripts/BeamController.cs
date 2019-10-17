f633fd1171f73dba71dc1c41c201f3844ce642b3
Peter <petercoole85@gmail.com>
2019-10-17 23:46:44 +0100

    changes to game files

---
+++ Assets/Scripts/BeamController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeamController : MonoBehaviour
+{
+	public float speed = 1.0f;
+	public float destroyTime = 6.5f;
+    private Rigidbody2D rb;
+    private Vector2 screenBounds;
+	
+	void Start () 
+	{
+        rb = this.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(-speed, 0);
+        screenBounds = Camera.main.ScreenToViewportPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));	
+    }
+
+    // Update is called once per frame
+   void Update () 
+    {
+        Destroy(GameObject.Find("Beam(Clone)"),destroyTime);	
+	}
+}	
