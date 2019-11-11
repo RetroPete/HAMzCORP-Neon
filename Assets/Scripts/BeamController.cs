04c4acfd46501f3d79537f5a85a0f08d451bf244
Peter <petercoole85@gmail.com>
2019-11-11 18:30:28 +0000

    poi

---
+++ Assets/Scripts/BeamController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeamController : MonoBehaviour
+{
+	public float speed = 1.0f;
+	public float destroyTime = 6f;
+    private Rigidbody2D rb;
+    private Vector2 screenBounds;
+	public GameObject beam; // This is where you reference the beam you want to destroy
+	
+	void Start () 
+	{
+        rb = this.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(-speed, 0);
+        screenBounds = Camera.main.ScreenToViewportPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));	
+    }
+
+    // Update is called once per frame
+    void Update () 
+    {
+        Destroy(GameObject.Find("Beam(Clone)"), destroyTime);	
+	}
+	
+	//void OnTriggerEnter2D(Collider2D other)
+	//{
+		//if(other.tag == "Player")
+		//{
+			//Destroy(GameObject.Find("Beam(Clone)"));
+		//}
+	//}
+}
---
+++ Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour
+{
+    public bool scrolling,paralax;
+
+    public float backgroundSize;
+    public float paralaxSpeed;
+
+    private Transform cameraTranform;
+    private Transform[] layers;
+    private float viewZone = 10;
+    private float lastCamraX;
+    private int leftIndex;
+    private int rightIndex;
+    
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        cameraTranform = Camera.main.transform;
+        lastCamraX = cameraTranform.position.x;
+        layers = new Transform[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+            layers[i] = transform.GetChild(i);
+
+        leftIndex = 0;
+        rightIndex = layers.Length - 1;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (paralax)
+        {
+            float deltaX = cameraTranform.position.x - lastCamraX;
+            transform.position += Vector3.right * (deltaX * paralaxSpeed);
+        }
+        
+        lastCamraX = cameraTranform.position.x;
+
+        if (scrolling)
+        {
+            if (cameraTranform.position.x < (layers[leftIndex].transform.position.x + viewZone))
+                ScrollLeft();
+
+            if (cameraTranform.position.x > (layers[rightIndex].transform.position.x - viewZone))
+                ScrollRight();
+        }
+    }
+
+    private void ScrollLeft()
+    {
+        int lastRight = rightIndex;
+        layers[rightIndex].position = Vector3.right * (layers[leftIndex].position.x - backgroundSize);
+        leftIndex = rightIndex;
+        rightIndex--;
+        if (rightIndex < 0)
+            rightIndex = layers.Length - 1;
+    }
+
+    private void ScrollRight()
+    {
+        int lastLeft = leftIndex;
+        layers[leftIndex].position = Vector3.right * (layers[rightIndex].position.x + backgroundSize);
+        rightIndex = leftIndex;
+        leftIndex++;
+        if (leftIndex == layers.Length)
+            leftIndex = 0;
+    }
+}
