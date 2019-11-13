53e585dd595729d015d6209ec5d36af462d9e550
Peter <petercoole85@gmail.com>
2019-11-13 19:37:06 +0000

    fixes

--- Assets/Scripts/SpawnBeam.cs
+++ Assets/Scripts/SpawnBeam.cs
@@ -8,7 +8,7 @@ public class SpawnBeam : MonoBehaviour
 	public GameObject[] beams;
 	//public GameObject beamPrefab;
 	//public Sprite[] beamSprites;
-    public float respawnTime = 4.0f;
+    public float respawnTime = 1.0f;
 	public GameObject player;
 	private Vector3 offset;
 
@@ -27,9 +27,10 @@ public class SpawnBeam : MonoBehaviour
 		//newBeam.transform.position = new Vector3(15 + player.transform.position.x + offset.x, Random.Range(-3.5f,3.5f), offset.z);
 		
 		beams = new GameObject[beamPrefabs.Length]; //makes sure they match length
-        for (int i = 0; i < beamPrefabs.Length; i++)
+        //for (int i = 0; i < beamPrefabs.Length; i++)
+		int i = Random.Range (0, beamPrefabs.Length);
         {
-            beams[i] = Instantiate(beamPrefabs[i]) as GameObject;
+			beams[i] = Instantiate(beamPrefabs[i]) as GameObject;
 			beams[i].transform.position = new Vector3(15 + player.transform.position.x + offset.x, Random.Range(-3.5f,3.5f), offset.z);
 		}
     }
