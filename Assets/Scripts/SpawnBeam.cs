d4d55bdd3b81659a96e6d388db85ffb963c9aaa9
Peter <34187808+RetroPete@users.noreply.github.com>
2019-12-09 17:48:45 +0000

    Changes to game files

--- Assets/Scripts/SpawnBeam.cs
+++ Assets/Scripts/SpawnBeam.cs
@@ -6,8 +6,6 @@ public class SpawnBeam : MonoBehaviour
 {
 	public GameObject[] beamPrefabs;
 	public GameObject[] beams;
-	//public GameObject beamPrefab;
-	//public Sprite[] beamSprites;
     public float respawnTime = 1.0f;
 	public GameObject player;
 	private Vector3 offset;
@@ -20,18 +18,12 @@ public class SpawnBeam : MonoBehaviour
 	
     private void spawnEnemy()
 	{
-        //int arrayIdx = Random.Range (0, beamSprites.Length);
-		//Sprite beamSprite = beamSprites[arrayIdx];
-		//GameObject newBeam = Instantiate(beamPrefab);
-		//newBeam.GetComponent<SpriteRenderer>().sprite = beamSprite;
-		//newBeam.transform.position = new Vector3(15 + player.transform.position.x + offset.x, Random.Range(-3.5f,3.5f), offset.z);
-		
-		beams = new GameObject[beamPrefabs.Length]; //makes sure they match length
-        //for (int i = 0; i < beamPrefabs.Length; i++)
+		beams = new GameObject[beamPrefabs.Length];
+        
 		int i = Random.Range (0, beamPrefabs.Length);
         {
 			beams[i] = Instantiate(beamPrefabs[i]) as GameObject;
-			beams[i].transform.position = new Vector3(25 + player.transform.position.x + offset.x, Random.Range(-3.5f,3.5f), offset.z);
+			beams[i].transform.position = new Vector3(10 + player.transform.position.x + offset.x, Random.Range(-1.5f,1.5f), offset.z);
 		}
     }
 	
