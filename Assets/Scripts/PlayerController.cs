aa4bb3f488a5e9154903fb82965ec434abd48fb6
Peter <34187808+RetroPete@users.noreply.github.com>
2020-03-15 22:14:49 +0000

    Changes to game files

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,14 +14,17 @@ public class PlayerController : MonoBehaviour
 	
 	public LevelManager theLevelManager;
 	
-	private Rigidbody2D body2d;
-	private Animator myAnim;
-
-    // Use this for initialization
+	private Animator animator;
+	
+	private Rigidbody2D rbody2D;
+	
+	
+   // Use this for initialization
     void Start()
     {
-        body2d = GetComponent<Rigidbody2D>();
-		myAnim = GetComponent<Animator>();
+        rbody2D = GetComponent<Rigidbody2D>();
+		
+		animator = GetComponent<Animator>();
 		
 		theLevelManager = FindObjectOfType<LevelManager>();
     }
@@ -29,21 +32,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxisRaw("Horizontal")>=0)
+        if (Input.GetAxisRaw("Horizontal") >=0)
 		{
 			horizontal = 0;
 			horizontal = Input.GetAxisRaw("Horizontal");
 			vertical = Input.GetAxisRaw("Vertical");
-		}
-
+		} 
+		
         Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
         pos.x = Mathf.Clamp01(pos.x);
         pos.y = Mathf.Clamp01(pos.y);
         transform.position = Camera.main.ViewportToWorldPoint(pos);
+		
+		animator.SetFloat("Vertical", Input.GetAxisRaw("Vertical"));
     }
 
     void FixedUpdate()
     {
-        body2d.velocity = new Vector2(autoMove + horizontal * moveSpeed, vertical * moveSpeed);
+        rbody2D.velocity = new Vector2(autoMove + horizontal * moveSpeed, vertical * moveSpeed);
     }
 }
